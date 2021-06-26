a9e8e9b01bc776dc53ad97ac8111a4c62a264f9e
Saidinesh777 <saidineshlankipalli777@gmail.com>
2021-06-26 07:54:34 +0530

    Added Code

--- Queues/linear_queue.cpp
+++ Queues/linear_queue.cpp
@@ -1,13 +1,99 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// class Queue{
+//     int arr[10];
+//     int front;
+//     int rear;
+//     int size;
+
+//     public:
+//     Queue(){
+//         front = rear = size = 0;
+//     }
+
+//     int get_size(){
+//         return size;
+//     }
+
+//     void push(int ele){     //Enables to push an element to the Queue
+//          if(size == 10){
+//              cout<<"Queue overflow"<<endl;
+//              return;
+//          }
+//          if(rear == 10){
+//              cout<<"Queue Saturated"<<endl;
+//              return;
+//          }
+
+//          arr[rear] = ele;
+//          rear++;
+
+//          size++;
+
+//     }
+
+//     void pop(){               //Enables to pop an element from the Queue
+//         if(size == 0){
+//             cout<<"Queue Underflow"<<endl;
+//             return;
+//         }
+
+        
+//         front++;
+//         size--;
+//     }
+
+//     int peek(){        //Enables to view,what is at the front of the Queue.
+//         if(size == 0){
+//             cout<<"Queue Empty"<<endl;
+//             return -1;
+//         }
+
+//         return arr[front];
+//     }
+
+
+// };
+
+// int main(){
+//     Queue q;
+//     for(int i = 1; i <=10; i++){
+//       q.push(i*10);
+//     }
+    
+//     q.push(110);
+
+//     cout<<"Size of Queue is:"<<q.get_size()<<endl;
+
+    
+//     for(int i = 1; i <=10; i++){
+//         cout<<q.peek()<<" ";
+//         q.pop();
+//     }
+
+//     cout<<endl;
+
+//      cout<<"Size of Queue is:"<<q.get_size()<<endl;
+
+//      q.push(10);
+
+//     return 0;
+// }
+
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
 class Queue{
-    public:
-     int arr[10];
+    int arr[10];
     int front;
     int rear;
     int size;
 
+    public:
     Queue(){
         front = rear = size = 0;
     }
@@ -16,67 +102,62 @@ class Queue{
         return size;
     }
 
-    void push(int ele){     //Enables to push an element to the Queue
-         if(size == 10){
-             cout<<"Queue overflow"<<endl;
-             return;
-         }
-         if(rear == 10){
-             cout<<"Queue Saturated"<<endl;
-             return;
-         }
+    void push(int ele){
+        if(size == 10){
+            cout<<"Queue Overflow"<<endl;
+            return;                   
+        }
 
-         arr[rear] = ele;
-         rear++;
-
-         size++;
-
+        if(rear == 10){
+            cout<<"Queue Saturated"<<endl;
+            return;
+        }
+        arr[rear] = ele;
+        rear++;
+        
+        size++;
     }
 
-    void pop(){               //Enables to pop an element from the Queue
+    void pop(){
         if(size == 0){
-            cout<<"Queue Underflow"<<endl;
+            cout<<"Queue Underflow";
             return;
         }
 
-        
         front++;
         size--;
+
     }
 
-    int peek(){        //Enables to view what is at the front of the Queue.
+    int peek(){
         if(size == 0){
             cout<<"Queue Empty"<<endl;
             return -1;
         }
-
         return arr[front];
     }
-
-
 };
 
 int main(){
     Queue q;
-    for(int i = 1; i <=10; i++){
-      q.push(i*10);
-    }
-    
+
+    for(int i = 1; i<= 10; i++)
+        q.push(i*10);
+
     q.push(110);
 
-    cout<<"Size of Queue is:"<<q.get_size()<<endl;
+    cout<<"Size of Queue:"<<q.get_size()<<endl;
 
-    
-    for(int i = 1; i <=10; i++){
+    for(int i = 1; i<=10; i++){
         cout<<q.peek()<<" ";
         q.pop();
     }
 
     cout<<endl;
 
-     cout<<"Size of Queue is:"<<q.get_size()<<endl;
+    cout<<"Size of Queue:"<<q.get_size()<<endl;
 
-     q.push(10);
+    q.push(10);
 
     return 0;
 }
