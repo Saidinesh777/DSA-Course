e98521d9b629eeb0c2b702529c76a4e7658860d7
Saidinesh777 <saidineshlankipalli777@gmail.com>
2021-07-28 20:03:00 +0530

    Added Code

--- DP/LCS.cpp
+++ DP/LCS.cpp
@@ -1,12 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int lcs(string A,string B){
-    vector<vector<int>> dp((A.size()+1), vector<int>(B.size()+1,0));
+int lcs(string A, string B){
+    vector<vector<int>> dp(A.size()+1, vector<int>(B.size()+1,0));
 
-    for(int i = 1; i<=A.size();i++){
-        for(int j = 1; j<=B.size();j++){
-            if(A[i-1] == B[j-1]){
+    for(int i = 1 ; i <= A.size() ; i++){
+        for(int j = 1 ; j <= B.size() ; j++){
+            if(A[i-1]==B[j-1]){
                 dp[i][j] = dp[i-1][j-1] + 1;
             }else{
                 dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
@@ -14,22 +14,16 @@ int lcs(string A,string B){
         }
     }
 
-    for(vector<int> row:dp){
-        for(int i:row){
+    // return dp.back().back();
+
+    for(auto row: dp){
+        for(int i: row){
             cout<<i<<" ";
         }
         cout<<endl;
     }
 
-    //   for(auto column:dp){
-    //     for(int j:column){
-    //         cout<<j<<" ";
-    //     }
-    //     cout<<endl;
-    // }
-
-     return dp[A.size()][B.size()];
-
+    return dp[A.size()][B.size()];
 }
 
 int main(){
