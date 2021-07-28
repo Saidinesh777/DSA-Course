95c4ef3d467a78ba2dda32e60fcc032b34f2b3b5
Saidinesh777 <saidineshlankipalli777@gmail.com>
2021-07-28 13:53:59 +0530

    Added Code

---
+++ DP/LCS.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int lcs(string A,string B){
+    vector<vector<int>> dp((A.size()+1), vector<int>(B.size()+1,0));
+
+    for(int i = 1; i<=A.size();i++){
+        for(int j = 1; j<=B.size();j++){
+            if(A[i-1] == B[j-1]){
+                dp[i][j] = dp[i-1][j-1] + 1;
+            }else{
+                dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
+            }
+        }
+    }
+
+    for(vector<int> row:dp){
+        for(int i:row){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+
+    //   for(auto column:dp){
+    //     for(int j:column){
+    //         cout<<j<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+     return dp[A.size()][B.size()];
+
+}
+
+int main(){
+    string A = "AGGTAB";
+    string B = "GXTXAYB";
+
+    cout<<lcs(A,B);
+
+    return 0;
+}
